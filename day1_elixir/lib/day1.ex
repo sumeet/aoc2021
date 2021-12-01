5cd0a85c6da632e143a6ea41b4614ad921cf2618
Sumeet Agarwal <sumeet.a@gmail.com>
2021-11-30 21:48:16 -0800

    rename dir

--- day1_elixir/lib/day1.ex
+++ day1_elixir/lib/day1.ex
@@ -2,11 +2,8 @@ defmodule Day1 do
   def part_1 do
     ints = File.read!("./input") |> String.split("\n") |> Enum.map(&String.to_integer/1)
 
-    seconds = Enum.drop(ints, 1) |> Enum.with_index()
-
-    Enum.map(seconds, fn {n, i} ->
-      if n > Enum.at(ints, i), do: 1, else: 0
-    end)
+    Enum.chunk_every(ints, 2, 1, :discard)
+    |> Enum.map(fn [a, b] -> if b > a, do: 1, else: 0 end)
     |> Enum.sum()
   end
 
@@ -14,15 +11,14 @@ defmodule Day1 do
     ints = File.read!("./input") |> String.split("\n") |> Enum.map(&String.to_integer/1)
     chunks = Enum.chunk_every(ints, 3, 1, :discard)
     chunk_sums = chunks |> Enum.map(&Enum.sum/1)
-    seconds = Enum.drop(chunk_sums, 1) |> Enum.with_index()
 
-    Enum.map(seconds, fn {n, i} ->
-      if n > Enum.at(chunk_sums, i), do: 1, else: 0
-    end)
+    Enum.chunk_every(chunk_sums, 2, 1, :discard)
+    |> Enum.map(fn [a, b] -> if b > a, do: 1, else: 0 end)
     |> Enum.sum()
   end
 
   def go do
+    IO.inspect(part_1())
     IO.inspect(part_2())
   end
 end
