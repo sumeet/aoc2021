af31aa0bcfc6a352f29ebe9bf590f6b1c087ddbf
Sumeet Agarwal <sumeet.a@gmail.com>
2021-12-13 20:02:12 -0800

    wip this might be it

--- day11_haskell/part1.hs
+++ day11_haskell/part1.hs
@@ -2,11 +2,18 @@ import Data.Char (digitToInt)
 import Data.List (intercalate)
 import Data.List.Extra ((!?))
 import Data.Maybe (catMaybes)
+import Debug.Trace (trace, traceShowId)
 
-dumpGrid :: [[Int]] -> IO ()
-dumpGrid rows = do
-  let x = map (map show) rows
-  putStrLn $ intercalate "\n" $ map (intercalate "") x
+untilStable :: Eq a => (a -> a) -> a -> a
+untilStable f x = if x == x' then x else untilStable f x'
+  where
+    x' = f x
+
+dumpGrid :: [[Int]] -> String
+dumpGrid rows = intercalate "\n" (map (intercalate "") rowStrings) ++ "\n"
+  where
+    rowStrings = map (map print) rows
+    print n = if n == -1 then "-" else show n
 
 neighbors :: [[Int]] -> Int -> Int -> [Int]
 neighbors grid x y =
@@ -17,26 +24,41 @@ neighbors grid x y =
         (dx, dy) /= (0, 0)
     ]
 
-nextGrid :: [[Int]] -> [[Int]]
-nextGrid grid =
-  [ [ nextCell x y
-      | (x, cell) <- zip [0 ..] row
+incrementStep :: [[Int]] -> [[Int]]
+incrementStep grid = [[cell + 1 | cell <- row] | row <- grid]
+
+flashingStep :: [[Int]] -> [[Int]]
+flashingStep grid =
+  [ [ nextCell x y thisCell
+      | (x, thisCell) <- zip [0 ..] row
     ]
     | (y, row) <- zip [0 ..] grid
   ]
   where
-    nextCell x y =
-      let current = (grid !! y) !! x
-          next' = current + 1
-          next'' = next' + length (filter (== 9) $ neighbors grid x y)
-       in if next'' > 9 then 0 else next''
+    nextCell x y thisCell =
+      if thisCell `elem` [0, -1]
+        then 0
+        else
+          let nbors = neighbors grid x y
+              numFlashingNeighbors = length (filter (== -1) nbors)
+              next = thisCell + numFlashingNeighbors
+           in if next > 9 then -1 else next
+
+zeroOutFlashes :: (Ord a, Num a) => [[a]] -> [[a]]
+zeroOutFlashes grid = [[max cell 0 | cell <- row] | row <- grid]
+
+nextGrid :: [[Int]] -> [[Int]]
+nextGrid = zeroOutFlashes . untilStable flashingStep . incrementStep
+
+traceWith :: (a -> String) -> a -> a
+traceWith f x = trace (f x) x
 
 main :: IO ()
 main = do
   s <- readFile "./sample"
   let grid = map (map digitToInt) $ lines s
-  dumpGrid grid
+  putStrLn $ dumpGrid grid
   putStrLn ""
-  dumpGrid $ nextGrid grid
+  putStrLn $ dumpGrid $ nextGrid grid
   putStrLn ""
-  dumpGrid $ nextGrid $ nextGrid grid
+  putStrLn $ dumpGrid $ nextGrid $ nextGrid grid
