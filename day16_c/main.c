639c47a668584956724973c6355432f1629ba176
Sumeet Agarwal <sumeet.a@gmail.com>
2021-12-16 16:32:46 -0800

    finish day 16.2, finally caught up

--- day16_c/main.c
+++ day16_c/main.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
+#include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/param.h>
 
 #define BUFFER_SIZE 1000
 
@@ -11,7 +13,7 @@ uint8_t input_left_in_byte = 8;
 
 uint64_t version_sum = 0;
 
-void parse_input(char* filename);
+void parse_input(char *filename);
 uint64_t parse_packet();
 
 size_t input_cursor_bits() {
@@ -33,16 +35,6 @@ uint64_t consume_bits(size_t n) {
     return acc;
 }
 
-uint64_t peek_bits(size_t n) {
-    size_t saved_input_i = input_i;
-    uint8_t saved_input_left_in_byte = input_left_in_byte;
-    uint64_t result = consume_bits(n);
-    input_i = saved_input_i;
-    input_left_in_byte = saved_input_left_in_byte;
-    return result;
-}
-
-
 void panic(const char *message) {
     printf("%s\n", message);
     exit(1);
@@ -60,25 +52,83 @@ uint64_t parse_literal_body() {
     return acc;
 }
 
-void parse_operator_body(uint8_t type_id) {
-    uint64_t length_type_id = consume_bits(1);
+size_t parse_subpackets_bitlength(uint64_t bitlength, uint64_t *subpackets) {
     size_t num_subpackets = 0;
+
+    size_t current_cursor = input_cursor_bits();
+    while (input_cursor_bits() - current_cursor < bitlength) {
+        subpackets[num_subpackets++] = parse_packet();
+    }
+    assert(input_cursor_bits() - current_cursor == bitlength);
+
+    return num_subpackets;
+}
+
+uint64_t parse_operator_body(uint8_t type_id) {
+    uint64_t length_type_id = consume_bits(1);
+    size_t num_subpackets;
     uint64_t subpackets[100];
     if (length_type_id == 0) {
         uint64_t length_of_subpackets = consume_bits(15);
-        size_t current_cursor = input_cursor_bits();
-        while (input_cursor_bits() - current_cursor < length_of_subpackets) {
-            subpackets[num_subpackets++] = parse_packet();
-        }
-        if (input_cursor_bits() - current_cursor != length_of_subpackets) {
-            printf("Expected to consume %lu bits, but consumed %lu bits\n",
-                   length_of_subpackets, input_cursor_bits() - current_cursor);
-            panic("operator innards length mismatch");
-        }
+        num_subpackets = parse_subpackets_bitlength(length_of_subpackets, subpackets);
     } else {
-        for (uint64_t i = consume_bits(11); i > 0; i--) {
+        num_subpackets = 0;
+        for (uint64_t rem = consume_bits(11); rem > 0; rem--) {
             subpackets[num_subpackets++] = parse_packet();
         }
+    }
+
+    switch (type_id) {
+        // sum
+        case 0: {
+            uint64_t acc = 0;
+            for (; num_subpackets > 0; num_subpackets--) {
+                acc += subpackets[num_subpackets - 1];
+            }
+            return acc;
+        }
+        // product
+        case 1: {
+            uint64_t acc = 1;
+            for (; num_subpackets > 0; num_subpackets--) {
+                acc *= subpackets[num_subpackets - 1];
+            }
+            return acc;
+        }
+        // min
+        case 2: {
+            uint64_t min = UINT64_MAX;
+            for (; num_subpackets > 0; num_subpackets--) {
+                min = MIN(min, subpackets[num_subpackets - 1]);
+            }
+            return min;
+        }
+        // max
+        case 3: {
+            uint64_t max = 0;
+            for (; num_subpackets > 0; num_subpackets--) {
+                max = MAX(max, subpackets[num_subpackets - 1]);
+            }
+            return max;
+        }
+        // greater than
+        case 5: {
+            assert(num_subpackets == 2);
+            return subpackets[0] > subpackets[1];
+        }
+        // less than
+        case 6: {
+            assert(num_subpackets == 2);
+            return subpackets[0] < subpackets[1];
+        }
+        // equal
+        case 7: {
+            assert(num_subpackets == 2);
+            return subpackets[0] == subpackets[1];
+        }
+        default:
+            printf("Unknown operator type id %u\n", type_id);
+            panic("unknown operator type");
     }
 }
 
@@ -93,16 +143,17 @@ uint64_t parse_packet() {
     uint8_t type_id = parse_header();
 
     if (type_id == 4) {
-        parse_literal_body();
+        return parse_literal_body();
     } else {
-        parse_operator_body(type_id);
+        return parse_operator_body(type_id);
     }
 }
 
 int main() {
     parse_input("input");
-    parse_packet();
+    uint64_t result = parse_packet();
     printf("Part 1: %lu\n", version_sum);
+    printf("Part 2: %lu\n", result);
     return 0;
 }
 
@@ -116,7 +167,7 @@ uint8_t nib_from_hex(char c) {
     }
 }
 
-void parse_input(char* filename) {
+void parse_input(char *filename) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         panic("couldn't find file");
